a5316f16d8d19105a63edd5c033c45a50a6c7e42
Ruslan <riybalkin8@gmail.com>
2019-03-21 17:55:34 +0200

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+var incr = 10;
+var decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+
+var isChecked = false;
+var isClose = false;
+ 
+console.log(isChecked || !isClose);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+var arr = ['0000','qqqq','wwww'];
+console.log(arr[0]);
